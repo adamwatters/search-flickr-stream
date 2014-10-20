d23e1d182a3cfe72619ae593cfcde4fd8596f914
Adam <adamwatters11@gmail.com>
2014-10-20 12:56:15 -0400

    server request issues resolved (no lag between search term entered and search data returned)

--- public/js/fetch.jsx
+++ public/js/fetch.jsx
@@ -29,37 +29,3 @@ var getPictures = function(callback, searchTerm) {
 	  	}
 	});
 };
-
-var getPicturesBySearch = function(callback, component, target) {
-
-	pictures = [];
-
-	$.ajax({
-	  	dataType: "text",
-	  	url: "/pics",
-	  	success: function(data){
-	  		data = data.replace(/\\'/g,"'");
-	  		data = JSON.parse(data);
-	 		for (var i = 0; i < data.items.length; i+=1){
-				pictures.push(data.items[i].media.m);
-			};
-	  	},
-	  	error: function(error, errorTwo){
-	  		alert(error, errorTwo);
-	  	},
-	  	complete: function(){
-	  		if (callback === undefined){
-	  			return;
-	  		}; 
-	  		callback(component, target);
-	  	}
-	});
-}
-
-
-
-
-
-
-
-
--- server.js
+++ server.js
@@ -2,35 +2,31 @@ var express = require("express");
 var app = express();
 var http = require("http");
 
-var pictureString = "";
-
-var callback = function(response) {
-  var str = '';
-
-  //another chunk of data has been recieved, so append it to `str`
-  response.on("data", function (chunk) {
-    str += chunk;
-  });
-
-  //the whole response has been recieved, so we just print it out here
-  response.on("end", function () {
-  	pictureString = str;
-  });
-};
-
 app.use('/', express.static(__dirname + '/public'));
 
 app.get("/pics", function(request, response){
   var queryString = request.query.searchTerm;
   var pathString = "/services/feeds/photos_public.gne?format=json&nojsoncallback=1&tags=" + queryString
-  var options = {
+  
+  var request = http.request({
     host: "api.flickr.com",
-    path: pathString
-  };
-  http.request(options, callback).end();
-	response.write(pictureString);
-	response.end();
+    path: pathString,
+    }, 
+    function(flickrResponse) {
+      var str = '';
+
+    //another chunk of data has been recieved, so append it to `str`
+      flickrResponse.on("data", function (chunk) {
+        str += chunk;
+    });
+
+    //the whole response has been recieved, so we just print it out here
+    flickrResponse.on("end", function () {
+      response.end(str);
+    });
+  });
+  request.end();
 });
 
-app.listen(8000);
-console.log("server listening on port 8000");
+app.listen(7000);
+console.log("server listening on port 7000");
